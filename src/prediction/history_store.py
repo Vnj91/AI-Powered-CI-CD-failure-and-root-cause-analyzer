7fdcdacc3c46dc6ef594a5bae68d4e9d96ee6c4a
Vanshaj Srivastava <srivastavavnj91@gmail.com>
2026-09-24 00:29:46 +0530

    Revert "test lint failure RCA"
    This reverts commit 8fe9efd9e59005382ca32be552c93a73a0e53ef2.

--- src/prediction/history_store.py
+++ src/prediction/history_store.py
@@ -336,6 +336,3 @@ def load_prediction_history(path: str | Path) -> "pd.DataFrame":
 def save_prediction_history(frame: "pd.DataFrame", path: str | Path) -> None:
     store = PredictionHistoryStore(path)
     store._save(frame)
-
-
-x=1
